99c5aa8fdbf66d6b934f3c07e01aad5372e4cda5
Debiprasad Das <debiprasadxd@protonmail.com>
2024-11-27 15:27:43 +0530

    Update start.ps1

--- scripts/start.ps1
+++ scripts/start.ps1
@@ -1,2 +1,2 @@
 $BaseUrl = "https://raw.githubusercontent.com/xorgram/xor/$((Invoke-RestMethod -Uri "https://api.github.com/repos/xorgram/xor/tags")[0].name)"
-deno run --allow-env --allow-net --allow-read --allow-run --allow-write --import-map=$BaseUrl/import_map.json -r $BaseUrl/main.ts
+deno run --allow-env --allow-net --allow-read --allow-run --allow-write --allow-import --import-map=$BaseUrl/import_map.json -r $BaseUrl/main.ts
